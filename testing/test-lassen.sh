52706fa342bef7ac14e6c942a57e49fc67ba2fcf
Michael T. Campbell <mtcampbe@illinois.edu>
2022-05-22 05:16:30 -0700

    Tweak testing scripts into place.

--- testing/test-lassen.sh
+++ testing/test-lassen.sh
@@ -4,11 +4,17 @@ EMIRGE_HOME=$1
 TESTING_RESULTS_FILE=$2
 TESTING_LOG_FILE=$3
 
-printf "Testing serial injection 3D with mixture EOS... \n"
-./test-serial-isolator-3d-mixture-lassen.sh ${EMIRGE_HOME}
-return_code=$?
-printf "Serial 3D mixture script return code: ${return_code}"
-cat serial-isolator-3d-mixture-output >> ${TESTING_LOG_FILE}
-test_result=$(cat serial-injection-3d-mixture-result)
-printf "serial-isolator-3d-mixture: ${test_result}\n" >> ${TESTING_RESULTS_FILE}
+printf "Isolator testing suite running on Lassen:\n"
+printf " - Emirge home: ${EMIRGE_HOME}\n"
+printf " - Testing results file: ${TESTING_RESULTS_FILE}\n"
+printf " - Testing log file: ${TESTING_LOG_FILE}\n"
+
+# Dispatch all the isolator-based tests from this script.
+# (1) 3d injection with mixture EOS (smoke_test_injection_3d)
+# - 1 GPU
+printf "Testing serial injection 3D with combustion... \n"
+./test-serial-3d-combustion-lassen.sh ${EMIRGE_HOME} ${TESTING_LOG_FILE} ${TESTING_RESULTS_FILE} "serial-3d-combustion"
+
 # Next - do parallel isolator tests (waits on parallel capability)
+# - 2 GPUs
+# - 4 GPUs
--- testing/test-serial-3d-combustion-lassen.sh
+++ testing/test-serial-3d-combustion-lassen.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 EMIRGE_HOME=$1
+TESTING_LOG_FILE=$2
+TESTING_RESULTS_FILE=$3
+TEST_NAME=$4
 
 origin=$(pwd)
-BATCH_SCRIPT_NAME="serial-3d-mixture-lassen-batch.sh"
+BATCH_SCRIPT_NAME="${TEST_NAME}-lassen-batch.sh"
 running_dir="${origin}/../smoke_test_injection_3d"
 
+printf "Running Lassen batch script Test ($TEST_NAME):\n"
+printf "- Origin directory: ${origin}\n"
+printf "- Running directory: ${running_dir}\n"
+printf "- Emirge home: ${EMIRGE_HOME}\n"
+printf "- Log file: ${TESTING_LOG_FILE}\n"
+printf "- Results file: ${TESTING_RESTULS_FILE}\n"
+
+# Most of the batch script stuff is boiler-plate
+# There is a short test-specific section marked below
+
+# - Generate a batch script to run the tests on a Lassen compute node
+# TODO: Generalize and create a utilty for generating the common batch stuff
 rm -rf ${BATCH_SCRIPT_NAME}
 cat <<EOF > ${BATCH_SCRIPT_NAME}
 #!/bin/bash
@@ -21,7 +36,7 @@ source "${EMIRGE_HOME}/config/activate_env.sh"
 export PYOPENCL_CTX="port:tesla"
 export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"
 rm -rf \$XDG_CACHE_HOME
-rm -f ${origin}/isolator-testing-done
+rm -f ${origin}/${TEST_NAME}-testing-done
 which python
 conda env list
 env
@@ -39,6 +54,8 @@ declare -i numsuccess=0
 
 cd ${running_dir}
 
+# VVVVVVVVV Test specific section VVVVVVVVVV
+
 echo "*** Running isolator in $running_dir ..."
 echo "Init phase...\n"
 \$serial_spawner_cmd python -O -u -m mpi4py ./isolator_injection_init.py -i run_params.yaml --lazy
@@ -50,33 +67,37 @@ then
    test_result="1"
 fi
 
-rm -f ${origin}/serial-isolator-3d-mixture-result
-printf "\${test_result}\n" > ${origin}/serial-isolator-3d-mixture-result
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-touch ${origin}/isolator-testing-done
+printf "${TEST_NAME}: \${test_result}\n" >> ${TESTING_RESULTS_FILE}
+
+touch ${origin}/${TEST_NAME}-testing-done
 exit \$numfail
 
 EOF
 
-rm -f isolator-testing-done
-chmod +x ${BATCH_SCRIPT_NAME}
 # ---- Submit the batch script and wait for the job to finish
+rm -f ${TEST_NAME}-testing-done
+chmod +x ${BATCH_SCRIPT_NAME}
 bsub ${BATCH_SCRIPT_NAME}
-# ---- Wait 10 minutes right off the bat
-sleep 600
+
+# ---- Wait 15 minutes right off the bat
+printf "Waiting for batch script to finish."
+sleep 900
 iwait=0
-while [ ! -f ./isolator-testing-done ]; do 
+while [ ! -f ./${TEST_NAME}-testing-done ]; do 
     iwait=$((iwait+1))
     if [ "$iwait" -gt 89 ]; then # give up after almost 1 hours
         printf "Timed out waiting on batch job.\n"
         exit 1 # skip the rest of the script
     fi
+    printf "."
     sleep 30
 done
-
+printf "\n"
 sleep 30  # give the batch system time to spew its junk into the log
-rm -rf serial-isolator-3d-mixture-output
-cat *.out > serial-isolator-3d-mixture-output
-date >> serial-isolator-3d-mixture-output
+printf "Output from Lassen batch job for test (${TEST_NAME}):\n" >> ${TESTING_LOG_FILE}
+cat *.out >> ${TESTING_LOG_FILE}
+date >> ${TESTING_LOG_FILE}
 rm *.out
 date
