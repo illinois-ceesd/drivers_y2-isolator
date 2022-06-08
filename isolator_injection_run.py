e54132dac19634a2b5f48f67798e7e8a03f127c0
Michael T. Campbell <mtcampbe@illinois.edu>
2022-06-08 06:56:22 -0700

    Dont try to use built-in pre-generated mech.

--- isolator_injection_run.py
+++ isolator_injection_run.py
@@ -586,7 +586,7 @@ def main(ctx_factory=cl.create_some_context,
         species_names = ["air", "fuel"]
     else:
         from mirgecom.thermochemistry import get_pyrometheus_wrapper_class
-        from mirgecom.mechanisms.uiuc import Thermochemistry
+        from uiuc import Thermochemistry
         pyro_mech = get_pyrometheus_wrapper_class(
             pyro_class=Thermochemistry, temperature_niter=pyro_temp_iter)(actx.np)
         eos = PyrometheusMixture(pyro_mech, temperature_guess=init_temperature)
