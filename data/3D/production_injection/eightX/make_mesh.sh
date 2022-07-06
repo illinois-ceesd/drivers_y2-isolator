9cdc50ad1f06271cbc6e91abd523b358685aab67
Mike Anderson <manders2@illinois.edu>
2022-07-06 07:55:34 -0700

    production meshes

---
+++ data/3D/production_injection/eightX/make_mesh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+NCPUS=$(getconf _NPROCESSORS_ONLN)
+
+gmsh -setnumber size 0.0064 -setnumber blratio 4 -setnumber cavityfac 2 -setnumber blratiocavity 2 -setnumber blratioinjector 2 -setnumber injectorfac 3 -setnumber shearfac 4 -o isolator.msh -nopopup -format msh2 ./isolator.geo -3 -nt $NCPUS
