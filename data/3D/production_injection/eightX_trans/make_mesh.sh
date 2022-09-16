b9d0f724a67bdb79a2fb4917abc455c6f33e4058
Mike Anderson <anderson2981@gmail.com>
2022-09-16 16:10:51 -0500

    added use_av: 3 to smoke_test_injection_2d

--- data/3D/production_injection/eightX_trans/make_mesh.sh
+++ data/3D/production_injection/eightX_trans/make_mesh.sh
@@ -4,4 +4,4 @@
 NCPUS=$(getconf _NPROCESSORS_ONLN)
 
 #gmsh -setnumber size 0.0064 -setnumber blratio 4 -setnumber cavityfac 4 -setnumber isofac 2 -setnumber blratiocavity 2 -setnumber blratioinjector 2 -setnumber injectorfac 4 -setnumber shearfac 4 -o isolator.msh -nopopup -format msh2 ./isolator_full.geo -3 -nt $NCPUS
-gmsh -setnumber blratio 2 -setnumber cavityfac 4 -setnumber isofac 2 -setnumber blratiocavity 2 -setnumber blratioinjector 2 -setnumber injectorfac 5 -setnumber shearfac 3 -setnumber scale 2 -setnumber scale_bl 1 -o isolator.msh -nopopup -format msh2 ./isolator_full.geo -3 -nt $NCPUS
+gmsh -setnumber blratio 2 -setnumber cavityfac 4 -setnumber isofac 2 -setnumber blratiocavity 2 -setnumber blratioinjector 2 -setnumber injectorfac 5 -setnumber shearfac 3 -setnumber scale 1 -setnumber scale_bl 1 -o isolator.msh -nopopup -format msh2 ./isolator_full.geo -3 -nt $NCPUS
